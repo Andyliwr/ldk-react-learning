f2f1f49a3759fce025f75b347dbc947e71b35f6b
lidikang <lidikang@myhexin.com>
2018-11-22 20:37:34 +0800

    upload build folder

---
+++ build/precache-manifest.e1e92d27ba798c98ce0398ae56f8f6e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77d376cb477c21da473a",
+    "url": "/static/js/main.77d376cb.chunk.js"
+  },
+  {
+    "revision": "9c50023bcdc1fbafb101",
+    "url": "/static/js/1.9c50023b.chunk.js"
+  },
+  {
+    "revision": "77d376cb477c21da473a",
+    "url": "/static/css/main.43c5db59.chunk.css"
+  },
+  {
+    "revision": "b39d95a26aa944ba0a64f089661f7cba",
+    "url": "/index.html"
+  }
+];
